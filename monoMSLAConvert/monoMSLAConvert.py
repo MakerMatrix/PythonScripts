c6748e5bbf715ca32aebfc576eb3a420ae69c10f
Jarrod Smith <maker.matrix.channel@gmail.com>
2020-09-04 22:00:02 -0500

    Fix formatting

--- monoMSLAConvert/monoMSLAConvert.py
+++ monoMSLAConvert/monoMSLAConvert.py
@@ -78,8 +78,8 @@ elif converting == G2C:
 		for y in range(outImg.height):  # Loop over the rows
 			# Build a new rgb-encoded pixel from the next three gray pixels
 			newPixel = (grayPixels[inX, y],
-						grayPixels[inX+1, y],
-						grayPixels[inX+2, y])
+					grayPixels[inX+1, y],
+					grayPixels[inX+2, y])
 			rgbPixels[x, y] = newPixel # Assign the rgb-encoded pixel
 
 # Save the converted image
